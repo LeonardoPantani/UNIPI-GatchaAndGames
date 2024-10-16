63ed3ad0aad9db10dc7879dbc727564bf6de3e96
Leonardo Pantani <leonardo.pantani@gmail.com>
2024-10-16 19:11:16 +0200

    Implementato register Aggiornati codici di errore per mostrare cosa si è sbagliato nell'invio del form

--- app/auth/auth.py
+++ app/auth/auth.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, request, jsonify, session, redirect, url_for, flash, render_template
 from werkzeug.security import generate_password_hash, check_password_hash
-from app import db
-from utils import emailCheck
+from app import db, app
+from utils import email_check, username_check
 import MySQLdb
 
 auth_bp = Blueprint('auth', __name__, template_folder='templates')
@@ -23,27 +23,14 @@ def register():
             return redirect(url_for('auth.register_page'))
         else: # se è una richiesta grezza
             return jsonify({"message": "All fields are mandatory."}), 400
-
-    if len(username) < 5:
-        if request.form:
-            flash("The username is invalid.", "error")
-            return redirect(url_for('auth.register_page'))
-        else:
-            return jsonify({"message": "The username is invalid."}), 400
-
-    if not emailCheck(email):
+    
+    # check email
+    if not email_check(email):
         if request.form:
             flash("The email is invalid.", "error")
             return redirect(url_for('auth.register_page'))
         else:
             return jsonify({"message": "The email is invalid."}), 400
-    
-    if password != repeat_password:
-        if request.form:
-            flash("The two passwords are not equal.", "error")
-            return redirect(url_for('auth.register_page'))
-        else:
-            return jsonify({"message": "The two passwords are not equal."}), 400
     
     try:
         cursor = db.connection.cursor()
@@ -56,17 +43,45 @@ def register():
             else:
                 return jsonify({"message": "That email is already in use."}), 409
         
+        # check username
+        if len(username) < 5:
+            if request.form:
+                flash("The username must be at least 5 characters long.", "error")
+                return redirect(url_for('auth.register_page'))
+            else:
+                return jsonify({"message": "The username must be at least 5 characters long."}), 400
+        
+        if not username_check(username):
+            if request.form:
+                flash("The username can only contain only letters, numbers and underscores.", "error")
+                return redirect(url_for('auth.register_page'))
+            else:
+                return jsonify({"message": "The username can only contain only letters, numbers and underscores."}), 400
+        
+        # check password
+        if password != repeat_password:
+            if request.form:
+                flash("The two passwords are not equal.", "error")
+                return redirect(url_for('auth.register_page'))
+            else:
+                return jsonify({"message": "The two passwords are not equal."}), 400
+        
+        # tutto ok, inserisco l'utente
         cursor.execute("INSERT INTO users (username, email, password) VALUES (%s, %s, %s)", 
-                       (username, email, generate_password_hash(password)))
+                    (username, email, generate_password_hash(password)))
         db.connection.commit()
         cursor.close()
         
+        # Se è tutto corretto, creo anche la sessione
         if request.form:
-            session['username'] = username
             flash("Register successful.", "success")
             return redirect(url_for('home.homepage'))
         else:
-            return jsonify({"message": "Register successful."}), 200
+            response = jsonify({"message": "Register successful."})
+            session_cookie = request.cookies.get(app.config["SESSION_COOKIE_NAME"])
+            if session_cookie:
+                response.set_cookie(app.config["SESSION_COOKIE_NAME"], session_cookie, httponly=True)
+            return response, 200
     except MySQLdb.Error as e:
         if request.form:
             flash("Internal error.", "error")
@@ -89,28 +104,51 @@ def login():
     password = data.get('password')
 
     if not username or not password:
-        flash("All fields are mandatory.", "error")
-        return redirect(url_for('auth.login_page'))
+        if request.form: # se la post è dal sito
+            flash("All fields are mandatory.", "error")
+            return redirect(url_for('auth.login_page'))
+        else: # se è una richiesta grezza
+            return jsonify({"message": "All fields are mandatory."}), 400
 
-    cursor = db.connection.cursor()
-    cursor.execute("SELECT username, password FROM users WHERE username = %s", (username,))
-    user = cursor.fetchone()
-    cursor.close()
+    try:
+        cursor = db.connection.cursor()
+        cursor.execute("SELECT username, password FROM users WHERE username = %s", (username,))
+        user = cursor.fetchone()
+        cursor.close()
+    except MySQLdb.Error as e:
+        if request.form:
+            flash("Internal error.", "error")
+            return redirect(url_for('auth.register_page'))
+        else:
+            return jsonify({"message": "Internal error."}), 500
 
     if user is None:
-        flash("That username was not found.", "error")
-        return redirect(url_for('auth.login_page'))
+        if request.form:
+            flash("Invalid credentials.", "error")
+            return redirect(url_for('auth.login_page'))
+        else:
+            return jsonify({"message": "Invalid credentials."}), 400
 
     stored_username, stored_password_hash = user
 
     if not check_password_hash(stored_password_hash, password):
-        flash("The password you entered is incorrect.", "error")
-        return redirect(url_for('auth.login_page'))
+        if request.form:
+            flash("Invalid credentials.", "error")
+            return redirect(url_for('auth.login_page'))
+        else:
+            return jsonify({"message": "Invalid credentials."}), 400
 
     # Se tutto è corretto, crea la sessione
     session['username'] = stored_username
-    flash("Login successful.", "success")
-    return redirect(url_for('home.homepage'))
+    if request.form:
+        flash("Login successful.", "success")
+        return redirect(url_for('home.homepage'))
+    else:
+        response = jsonify({"message": "Login successful."})
+        session_cookie = request.cookies.get(app.config["SESSION_COOKIE_NAME"])
+        if session_cookie:
+            response.set_cookie(app.config["SESSION_COOKIE_NAME"], session_cookie, httponly=True)
+        return response, 200
 
 @auth_bp.route('/login', methods=['GET'])
 def login_page():
@@ -122,18 +160,3 @@ def logout():
     session.pop('username', None)
     flash("Logout successful.", "success")
     return redirect(url_for('home.homepage'))
-
-
-@auth_bp.route('/reset_password', methods=['POST'])
-def reset_password():
-    data = request.get_json()
-    email = data.get('email')
-
-    if not email:
-        return jsonify({"error": "L'email è obbligatoria"}), 400
-    
-    if not emailCheck(email):
-        return jsonify({"error": "L'email fornita non è valida."}), 400
-
-    # Reset password qui
-    return jsonify({"message": "Istruzioni per il recupero della password inviate"}), 200
