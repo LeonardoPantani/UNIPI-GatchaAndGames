b0d8b3c26288865cd827513e4ee6781a4e6bf58a
Leonardo Pantani <leonardo.pantani@gmail.com>
2024-11-08 11:59:59 +0100

    notworking: added database container files

---
+++ database/init.sql
@@ -0,0 +1,2 @@
+GRANT ALL PRIVILEGES ON gacha_test_db.* TO 'gacha_test_user'@'%' IDENTIFIED BY 'gacha_test_password';
+FLUSH PRIVILEGES;
--- openapi_server/__main__.py
+++ openapi_server/__main__.py
@@ -3,16 +3,46 @@
 import connexion
 
 from openapi_server import encoder
+from dotenv import find_dotenv, dotenv_values
+from flask_mysqldb import MySQL
+import MySQLdb
+
 
 
 def main():
-    app = connexion.App(__name__, specification_dir='./openapi/')
-    app.app.json_encoder = encoder.JSONEncoder
-    app.add_api('openapi.yaml',
+    connexion_app = connexion.App(__name__, specification_dir='./openapi/')
+    connexion_app.app.json_encoder = encoder.JSONEncoder
+    connexion_app.add_api('openapi.yaml',
                 arguments={'title': 'Gacha System - OpenAPI 3.0'},
                 pythonic_params=True)
+    
+    # Carico le variabili da .env
+    config = dotenv_values(find_dotenv())
+    print(config)
 
-    app.run(port=8080, debug=True)
+    connexion_app.app.config["MYSQL_HOST"] = config["MYSQL_HOST"]
+    connexion_app.app.config["MYSQL_USER"] = config["MYSQL_USER"]
+    connexion_app.app.config["MYSQL_PASSWORD"] = config["MYSQL_PASSWORD"]
+    connexion_app.app.config["MYSQL_DB"] = config["MYSQL_DB"]
+    connexion_app.app.secret_key = config["FLASK_SECRET_KEY"]
+    
+    # Preparo MYSQL
+    db = MySQL(connexion_app.app)
+
+    # Controllo la connessione al database
+    try:
+        conn = MySQLdb.connect(
+            host=config["MYSQL_HOST"],
+            user=config["MYSQL_USER"],
+            password=config["MYSQL_PASSWORD"],
+            db=config["MYSQL_DB"]
+        )
+        conn.close()
+    except MySQLdb.OperationalError as e:
+        print(f"[!] Errore di connessione al database: {e}")
+        quit()
+
+    connexion_app.run(port=8080, debug=True)
 
 
 if __name__ == '__main__':
