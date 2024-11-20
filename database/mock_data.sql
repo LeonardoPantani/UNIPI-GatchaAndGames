18e6a5e3780d4eec11eb5edfdc2980eecac896d0
rickardof <riccardofantasia1@gmail.com>
2024-11-20 21:30:08 +0100

    Aggiunti mock data per pvp

--- database/mock_data.sql
+++ database/mock_data.sql
@@ -5,7 +5,7 @@ INSERT INTO users (uuid, email, password, role) VALUES
 (UNHEX(REPLACE('a4f0c592-12af-4bde-aacd-94cd0f27c57e', '-', '')), 'giorno@passione.it', '$2b$12$jqb2pkG1hQ4j4xJkbYTt8el9iVraf9IOg5ODWR9cjHcVF9sttNTja', 'USER'), -- password: gold_experience
 (UNHEX(REPLACE('b5c3d2e1-4f5e-6a7b-8c9d-0e1f2a3b4c5d', '-', '')), 'josuke@morioh.jp', '$2b$12$jqb2pkG1hQ4j4xJkbYTt8el9iVraf9IOg5ODWR9cjHcVF9sttNTja', 'USER'), -- password: crazy_diamond
 (UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')), 'speedwagon@foundation.org', '$2a$12$9HgqzV4s6zhKCBFRMuUGSONqS2bhIQqzpiF1U/K/VW1ofYWyU2mIa', 'ADMIN'), -- password: admin_foundation
-(UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')), 'admin@admin.com', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewqkfQhrLvvD3.4K', 'ADMIN'); -- password: password -- Insert additional admin user
+(UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')), 'admin@admin.com', '$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewqkfQhrLvvD3.4K', 'ADMIN'); -- password: password -- additional admin user
 
 
 -- Insert into `profiles` table
@@ -109,15 +109,69 @@ INSERT INTO inventories (item_uuid, owner_uuid, stand_uuid, obtained_at, owners_
  UNHEX(REPLACE('b6e7f8c9-7f28-4b4f-8fbe-523f6c8b0c85', '-', '')), -- Gold Experience
  '2024-01-03', 1, 2500);
 
- INSERT INTO inventories (item_uuid, owner_uuid, stand_uuid, obtained_at, owners_no, currency_spent) VALUES
+-- Speedwagon's Inventory (7 Stands)
+INSERT INTO inventories (item_uuid, owner_uuid, stand_uuid, obtained_at, owners_no, currency_spent) VALUES
 (UNHEX(REPLACE('c7b6a5d4-e3f2-1098-7654-fedcba987654', '-', '')), 
  UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')), -- Speedwagon
- UNHEX(REPLACE('e7f8a9b0-c1d2-3e4f-5a6b-7c8d9e0f1a2b', '-', '')), -- Magicians Red 
+ UNHEX(REPLACE('1b2f7b4e-5e1f-4112-a7c5-b7559dbb8c76', '-', '')), -- Star Platinum (LEGENDARY)
  '2024-01-01', 1, 5000),
 (UNHEX(REPLACE('b7a6c5d4-e3f2-1098-7654-fedcba987655', '-', '')),
- UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')), -- Speedwagon
- UNHEX(REPLACE('c1d2e3f4-5a6b-7c8d-9e0f-1a2b3c4d5e6f', '-', '')), -- Silver Chariot
- '2024-01-02', 1, 6000);
+ UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')),
+ UNHEX(REPLACE('9d4b9fa9-6c72-44f5-9ac6-e6b548cfc632', '-', '')), -- The World (LEGENDARY)
+ '2024-01-02', 1, 5000),
+(UNHEX(REPLACE('a7b6c5d4-e3f2-1098-7654-fedcba987656', '-', '')),
+ UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')),
+ UNHEX(REPLACE('b6e7f8c9-7f28-4b4f-8fbe-523f6c8b0c85', '-', '')), -- Gold Experience (EPIC)
+ '2024-01-03', 1, 3000),
+(UNHEX(REPLACE('97b6c5d4-e3f2-1098-7654-fedcba987657', '-', '')),
+ UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')),
+ UNHEX(REPLACE('8f2b9d4e-3b3e-4c5d-a1c6-b7e3a5d6c1c7', '-', '')), -- Crazy Diamond (EPIC)
+ '2024-01-04', 1, 3000),
+(UNHEX(REPLACE('87b6c5d4-e3f2-1098-7654-fedcba987658', '-', '')),
+ UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')),
+ UNHEX(REPLACE('c1d2e3f4-5a6b-7c8d-9e0f-1a2b3c4d5e6f', '-', '')), -- Silver Chariot (RARE)
+ '2024-01-05', 1, 2000),
+(UNHEX(REPLACE('77b6c5d4-e3f2-1098-7654-fedcba987659', '-', '')),
+ UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')),
+ UNHEX(REPLACE('d4e5f6a7-b8c9-0d1e-2f3a-4b5c6d7e8f9a', '-', '')), -- Hermit Purple (COMMON)
+ '2024-01-06', 1, 1000),
+(UNHEX(REPLACE('67b6c5d4-e3f2-1098-7654-fedcba987660', '-', '')),
+ UNHEX(REPLACE('4f2e8bb5-38e1-4537-9cfa-11425c3b4284', '-', '')),
+ UNHEX(REPLACE('e7f8a9b0-c1d2-3e4f-5a6b-7c8d9e0f1a2b', '-', '')), -- Magicians Red (RARE)
+ '2024-01-07', 1, 2000);
+
+-- AdminUser's Inventory (7 different Stands)
+INSERT INTO inventories (item_uuid, owner_uuid, stand_uuid, obtained_at, owners_no, currency_spent) VALUES
+(UNHEX(REPLACE('57b6c5d4-e3f2-1098-7654-fedcba987661', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')), -- AdminUser
+ UNHEX(REPLACE('c3d4e5f6-a7b8-9012-3456-7890abcdef12', '-', '')), -- Made in Heaven (LEGENDARY)
+ '2024-01-08', 1, 5000),
+(UNHEX(REPLACE('47b6c5d4-e3f2-1098-7654-fedcba987662', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')),
+ UNHEX(REPLACE('a1b2c3d4-e5f6-7890-1234-567890abcdef', '-', '')), -- King Crimson (LEGENDARY)
+ '2024-01-09', 1, 5000),
+(UNHEX(REPLACE('37b6c5d4-e3f2-1098-7654-fedcba987663', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')),
+ UNHEX(REPLACE('b2c3d4e5-f6a7-8901-2345-67890abcdef1', '-', '')), -- Killer Queen (EPIC)
+ '2024-01-10', 1, 3000),
+(UNHEX(REPLACE('27b6c5d4-e3f2-1098-7654-fedcba987664', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')),
+ UNHEX(REPLACE('e5f6a7b8-c9d0-1234-5678-90abcdef1234', '-', '')), -- Purple Haze (EPIC)
+ '2024-01-11', 1, 3000),
+(UNHEX(REPLACE('17b6c5d4-e3f2-1098-7654-fedcba987665', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')),
+ UNHEX(REPLACE('f6a7b8c9-d0e1-2345-6789-0abcdef12345', '-', '')), -- Sex Pistols (RARE)
+ '2024-01-12', 1, 2000),
+(UNHEX(REPLACE('07b6c5d4-e3f2-1098-7654-fedcba987666', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')),
+ UNHEX(REPLACE('c9d0e1f2-a3b4-5678-9012-cdef12345678', '-', '')), -- Beach Boy (COMMON)
+ '2024-01-13', 1, 1000),
+(UNHEX(REPLACE('f6b6c5d4-e3f2-1098-7654-fedcba987667', '-', '')),
+ UNHEX(REPLACE('a1a2a3a4-b5b6-c7c8-d9d0-e1e2e3e4e5e6', '-', '')),
+ UNHEX(REPLACE('a7b8c9d0-e1f2-3456-7890-abcdef123456', '-', '')), -- Aerosmith (RARE)
+ '2024-01-14', 1, 2000);
+
+
 
  INSERT INTO pvp_matches (match_uuid, player_1_uuid, player_2_uuid, winner, match_log, timestamp) VALUES
 -- Jotaro vs DIO
