e8aa56ce3a1704f5b534189d68fa123c67390f38
ChristianSabella <christian.sabella1708@gmail.com>
2024-11-29 13:11:40 +0100

    feat: implemented currency_internal

--- services/currency/openapi_server/controllers/currency_internal_controller.py
+++ services/currency/openapi_server/controllers/currency_internal_controller.py
@@ -3,110 +3,362 @@ from typing import Dict
 from typing import Tuple
 from typing import Union
 
+from flask import jsonify, session
+import requests
+import logging
+
 from openapi_server.models.bundle import Bundle  # noqa: E501
 from openapi_server.models.get_user_history200_response import GetUserHistory200Response  # noqa: E501
 from openapi_server import util
 
+from pybreaker import CircuitBreaker, CircuitBreakerError
+
+from mysql.connector.errors import (
+    OperationalError, DataError, DatabaseError, IntegrityError,
+    InterfaceError, InternalError, ProgrammingError
+)
+from openapi_server.helpers.db import get_db
+
+circuit_breaker = CircuitBreaker(fail_max=1000, reset_timeout=5)
 
 def delete_user_transactions(session=None, uuid=None):  # noqa: E501
-    """delete_user_transactions
+    if not uuid:
+        return "", 400
+    
+    try:
+        @circuit_breaker
+        def delete_transactions():
+            connection = get_db()
+            cursor = connection.cursor()
 
-    Deletes transaction made by the user by UUID, if exist. # noqa: E501
+            query = """
+                DELETE 
+                FROM bundles_transactions
+                WHERE user_uuid = UUID_TO_BIN(%s)
+            """
 
-    :param session: 
-    :type session: str
-    :param uuid: 
-    :type uuid: str
-    :type uuid: str
+            cursor.execute(query, (uuid,))
 
-    :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
-    """
-    return 'do some magic!'
+            query = """
+                DELETE 
+                FROM ingame_transactions
+                WHERE user_uuid = UUID_TO_BIN(%s)
+            """
 
+            cursor.execute(query, (uuid,))
+
+            cursor.close()
+
+            connection.commit()
+
+        delete_transactions()
+
+        return jsonify({"message": "Transactions deleted."}), 200
+
+    except OperationalError:
+        logging.error(f"Query: Operational error.")
+        return "", 503
+    except ProgrammingError:
+        logging.error(f"Query: Programming error.")
+        return "", 503
+    except DataError:
+        logging.error(f"Query: Invalid data error.")
+        return "", 503 
+    except IntegrityError:
+        logging.error(f"Query: Integrity error.")
+        return "", 503
+    except DatabaseError:
+        logging.error(f"Query: Generic database error.")
+        return "", 503
+    except CircuitBreakerError:
+        return "", 503
 
 def get_bundle(session=None, codename=None):  # noqa: E501
-    """get_bundle
+    if not codename:
+        "", 400
 
-    Returns requested bundle info. # noqa: E501
+    try:
+        @circuit_breaker
+        def get_bundle_info():
+            connection = get_db()
+            cursor = connection.cursor()
 
-    :param session: 
-    :type session: str
-    :param codename: 
-    :type codename: str
+            query = """
+                SELECT codename, public_name, credits_obtained, currency_name, price
+                FROM bundles
+                WHERE codename = %s
+            """
 
-    :rtype: Union[Bundle, Tuple[Bundle, int], Tuple[Bundle, int, Dict[str, str]]
-    """
-    return 'do some magic!'
+            cursor.execute(query, (codename,))
 
+            bundle_data = cursor.fetchall()
+            
+            cursor.close()
+
+            if not bundle_data:
+                return None
+   
+            return bundle_data[0]
+        
+        bundle_info = get_bundle_info()
+
+        if not bundle_info:
+            return "", 404
+
+        payload = {
+            "codename": bundle_info[0],
+            "public_name": bundle_info[1],
+            "amount": bundle_info[2],
+            "currency": bundle_info[3],
+            "value": bundle_info[4]
+        }
+
+        return jsonify(payload), 200
+
+    except OperationalError:
+        logging.error(f"Query: Operational error.")
+        return "", 503
+    except ProgrammingError:
+        logging.error(f"Query: Programming error.")
+        return "", 503
+    except DataError:
+        logging.error(f"Query: Invalid data error.")
+        return "", 503 
+    except IntegrityError:
+        logging.error(f"Query: Integrity error.")
+        return "", 503
+    except DatabaseError as e:
+        logging.error(f"Query: Generic database error.")
+        return "", 503
+    except CircuitBreakerError:
+        return "", 503
 
 def get_user_history(session=None, uuid=None, history_type=None, page_number=None):  # noqa: E501
-    """Returns history of a user.
+    if not uuid or not history_type:
+        return "", 400
+    
+    if history_type != "bundle" and history_type!= "ingame":
+        return "", 400
+    
+    items_per_page = 10
+    offset = (page_number - 1) * items_per_page
 
-    Allows to retrieve history of a user. # noqa: E501
+    try:
+        @circuit_breaker
+        def get_user_transactions():
+            connection = get_db()
+            cursor = connection.cursor()
 
-    :param session: 
-    :type session: str
-    :param uuid: 
-    :type uuid: str
-    :type uuid: str
-    :param history_type: Type of history to show.
-    :type history_type: str
-    :param page_number: Page number of the list.
-    :type page_number: int
+            if history_type == "bundle":
+                query = """
+                    SELECT BIN_TO_UUID(user_uuid), timestamp, bundle_codename, bundle_currency_name
+                    FROM bundles_transactions
+                    WHERE user_uuid = UUID_TO_BIN(%s)
+                    LIMIT %s
+                    OFFSET %s
+                """
+            else:
+                query = """
+                    SELECT BIN_TO_UUID(user_uuid), timestamp, credits, transaction_type
+                    FROM ingame_transactions
+                    WHERE user_uuid = UUID_TO_BIN(%s)
+                    LIMIT %s
+                    OFFSET %s
+                """
 
-    :rtype: Union[GetUserHistory200Response, Tuple[GetUserHistory200Response, int], Tuple[GetUserHistory200Response, int, Dict[str, str]]
-    """
-    return 'do some magic!'
+            cursor.execute(query, (uuid, items_per_page, offset))
+
+            transaction_data = cursor.fetchall()
+
+            cursor.close()
+
+            return transaction_data
+        
+        transaction_list = get_user_transactions()
+
+        response = []
+
+        for transaction in transaction_list:
+            if history_type == "bundle":
+                payload = {
+                    "user_uuid": transaction[0],
+                    "timestamp": transaction[1],
+                    "codename": transaction[2],
+                    "currency_name": transaction[3]
+                }
+            else:
+                payload = {
+                    "user_uuid": transaction[0],
+                    "timestamp": transaction[1],
+                    "credits": transaction[2],
+                    "transaction_type": transaction[3]
+                }
+            response.append(payload)
+        print (response)
+        return jsonify(response), 200
+
+    except OperationalError:
+        logging.error(f"Query: Operational error.")
+        return "", 503
+    except ProgrammingError:
+        logging.error(f"Query: Programming error.")
+        return "", 503
+    except DataError:
+        logging.error(f"Query: Invalid data error.")
+        return "", 503 
+    except IntegrityError:
+        logging.error(f"Query: Integrity error.")
+        return "", 503
+    except DatabaseError:
+        logging.error(f"Query: Generic database error.")
+        return "", 503
+    except CircuitBreakerError:
+        return "", 503    
 
 
 def insert_bundle_transaction(session=None, uuid=None, bundle_codename=None, currency_name=None):  # noqa: E501
-    """insert_bundle_transaction
+    if not uuid or not bundle_codename or not currency_name:
+        return "", 400
+    
+    try:
+        @circuit_breaker
+        def insert_transaction():
+            connection = get_db()
+            cursor = connection.cursor()
 
-    Inserts transaction of ingame currency. # noqa: E501
+            query = """
+                INSERT INTO bundles_transactions
+                (bundle_codename, bundle_currency_name, user_uuid)
+                VALUES (%s, %s, UUID_TO_BIN(%s))
+            """
 
-    :param session: 
-    :type session: str
-    :param uuid: 
-    :type uuid: str
-    :type uuid: str
-    :param bundle_codename: Codename of the bundle.
-    :type bundle_codename: str
-    :param currency_name: Type of transaction.
-    :type currency_name: str
+            cursor.execute(query, (bundle_codename, currency_name, uuid))
 
-    :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
-    """
-    return 'do some magic!'
+            cursor.close()
 
+            connection.commit()
+
+        insert_transaction()
+
+        return jsonify({"message": "Transaction inserted."}), 200
+
+    except OperationalError:
+        logging.error(f"Query: Operational error.")
+        return "", 503
+    except ProgrammingError:
+        logging.error(f"Query: Programming error.")
+        return "", 503
+    except DataError:
+        logging.error(f"Query: Invalid data error.")
+        return "", 503 
+    except IntegrityError:
+        logging.error(f"Query: Integrity error.")
+        return "", 503
+    except DatabaseError:
+        logging.error(f"Query: Generic database error.")
+        return "", 503
+    except CircuitBreakerError:
+        return "", 503    
 
 def insert_ingame_transaction(session=None, uuid=None, current_bid=None, transaction_type=None):  # noqa: E501
-    """insert_ingame_transaction
+    if not uuid or not current_bid or not transaction_type:
+        return "", 400
+    
+    if transaction_type != "bought_bundle" and transaction_type != "sold_market" and transaction_type != "bought_market" and transaction_type != "gacha_pull":
+        return "", 400
 
-    Inserts transaction of ingame currency. # noqa: E501
+    try:
+        @circuit_breaker
+        def insert_transaction():
+            connection = get_db()
+            cursor = connection.cursor()
 
-    :param session: 
-    :type session: str
-    :param uuid: 
-    :type uuid: str
-    :type uuid: str
-    :param current_bid: Amount of currency spent.
-    :type current_bid: int
-    :param transaction_type: Type of transaction.
-    :type transaction_type: str
+            query = """
+                INSERT INTO ingame_transactions
+                (user_uuid, credits, transaction_type)
+                VALUES (UUID_TO_BIN(%s), %s, %s)
+            """
 
-    :rtype: Union[None, Tuple[None, int], Tuple[None, int, Dict[str, str]]
-    """
-    return 'do some magic!'
+            cursor.execute(query, (uuid, current_bid, transaction_type))
+
+            cursor.close()
+
+            connection.commit()
+
+        insert_transaction()
+
+        return jsonify({"message": "Transaction inserted."}), 200
+
+    except OperationalError:
+        logging.error(f"Query: Operational error.")
+        return "", 503
+    except ProgrammingError:
+        logging.error(f"Query: Programming error.")
+        return "", 503
+    except DataError:
+        logging.error(f"Query: Invalid data error.")
+        return "", 503 
+    except IntegrityError:
+        logging.error(f"Query: Integrity error.")
+        return "", 503
+    except DatabaseError:
+        logging.error(f"Query: Generic database error.")
+        return "", 503
+    except CircuitBreakerError:
+        return "", 503    
 
 
 def list_bundles(session=None):  # noqa: E501
-    """list_bundles
+    
+    try:
+        @circuit_breaker
+        def get_bundles():
+            connection = get_db()
+            cursor = connection.cursor()
 
-    Returns bundle list. # noqa: E501
+            query = """
+                SELECT codename, public_name, credits_obtained, currency_name, price
+                FROM bundles
+            """
 
-    :param session: 
-    :type session: str
+            cursor.execute(query)
 
-    :rtype: Union[List[Bundle], Tuple[List[Bundle], int], Tuple[List[Bundle], int, Dict[str, str]]
-    """
-    return 'do some magic!'
+            bundles_data = cursor.fetchall()
+
+            cursor.close()
+
+            return bundles_data
+        
+        bundles_list = get_bundles()
+
+        response = []
+
+        for bundle in bundles_list:
+            payload = {
+                "codename": bundle[0],
+                "public_name": bundle[1],
+                "amount": bundle[2],
+                "currency": bundle[3],
+                "value": bundle[4]
+            }
+            response.append(payload)
+
+        return jsonify(response), 200
+
+    except OperationalError:
+        logging.error(f"Query: Operational error.")
+        return "", 503
+    except ProgrammingError:
+        logging.error(f"Query: Programming error.")
+        return "", 503
+    except DataError:
+        logging.error(f"Query: Invalid data error.")
+        return "", 503 
+    except IntegrityError:
+        logging.error(f"Query: Integrity error.")
+        return "", 503
+    except DatabaseError:
+        logging.error(f"Query: Generic database error.")
+        return "", 503
+    except CircuitBreakerError:
+        return "", 503    
