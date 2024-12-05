470cb838c25828269a6fd0433846f7398082e819
Leonardo Pantani <leonardo.pantani@gmail.com>
2024-12-05 18:18:00 +0100

    fix(currency): now does not return error when more than one request is sent in one second

--- databases/currency/primary/init.sql
+++ databases/currency/primary/init.sql
@@ -13,11 +13,12 @@ CREATE TABLE bundles (
 -- launch_event_bundle_novice [100 EUR or 110 USD or 450 PLN] -> 1000 coins
 -- launch_event_bundle_pro [300 EUR or 350 USD or 1300 PLN]  -> 100000 coins
 CREATE TABLE bundles_transactions (
+    id INT AUTO_INCREMENT,
     bundle_codename VARCHAR(100),
     bundle_currency_name VARCHAR(3),
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     user_uuid BINARY(16),
-    PRIMARY KEY (bundle_codename, timestamp, user_uuid),
+    PRIMARY KEY (id),
     FOREIGN KEY (bundle_codename) REFERENCES bundles(codename)
 );
 
@@ -27,11 +28,12 @@ CREATE TABLE bundles_transactions (
 -- > user buys something from the market (credits deduced, -)
 -- > user pulls a gacha from a pool (credits deduced, -)
 CREATE TABLE ingame_transactions (
+    id INT AUTO_INCREMENT,
     user_uuid BINARY(16),
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     credits INT NOT NULL,
     transaction_type ENUM('bought_bundle', 'sold_market', 'bought_market', 'gacha_pull') NOT NULL,
-    PRIMARY KEY (user_uuid, timestamp)
+    PRIMARY KEY (id)
 );
 
 
